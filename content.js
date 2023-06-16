b821f3cea5ef627e8f989f80b1c8014607b2f2b3
Henry Li <henry_li@web.de>
2023-06-16 12:18:39 +0100

    Update

--- content.js
+++ content.js
@@ -12,6 +12,8 @@ window.addEventListener('load', () => {
 
 const selector = `[d="M10.65,1C10.65,1,10.65,1,10.65,1c-0.37,0-0.75,0.1-1.09,0.31L4.25,4.46C3.44,4.93,2.96,5.89,3,6.9  C3.05,7.9,3.58,8.77,4.39,9.18c0.02,0.01,0.75,0.35,0.75,0.35l-0.9,0.53c-1.14,0.68-1.58,2.27-0.98,3.55C3.69,14.49,4.5,15,5.35,15  c0.37,0,0.74-0.1,1.09-0.31l5.31-3.15c0.8-0.48,1.29-1.43,1.24-2.45c-0.04-0.99-0.58-1.87-1.39-2.27c-0.02-0.01-0.75-0.35-0.75-0.35  l0.9-0.53c1.14-0.68,1.58-2.27,0.97-3.55C12.31,1.51,11.49,1,10.65,1L10.65,1z"]`;
 
+const parent = "ytd-rich-item-renderer";
+
 const hook = () => {
 	if (!window.location.href.includes("/feed/subscriptions")) {
 		return;
@@ -19,7 +21,7 @@ const hook = () => {
 
 	if (hide) {
 		$(selector)
-			.parents("ytd-grid-video-renderer")
+			.parents(parent)
 			.hide()
 		;
 	}
@@ -30,7 +32,7 @@ const hotkeys = () => {
 		if ((e.metaKey || e.ctrlKey) && e.code === 'KeyS') {
 			e.preventDefault();
 			$(selector)
-				.parents("ytd-grid-video-renderer")
+				.parents(parent)
 				.toggle()
 			;
 			hide = !hide;
